4b1ffcec712d31cbda5965a139803c2f2f19803d
Mariappsetl <dheeraj.balakrishnan@mariapps.com>
2025-04-25 00:08:27 +0530

    Update chatbot_web.py

--- chatbot_web.py
+++ chatbot_web.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="MA-BI Team Chatbot", layout="centered")
 
-st.image("https://i.ibb.co/zTdj4ZRm/ma-bi-chatbot.png", width=80)
+st.image("https://cdn.pixabay.com/photo/2016/12/13/21/20/alien-1905155_640.png", width=80)
 st.markdown("### MA-BI Team Chatbot")
 
 user_input = st.text_input("Ask me about CMA or BSM (type 'exit' to quit):")
