2040203fcec3eaf526588363065b1f56883ab218
Mariappsetl <dheeraj.balakrishnan@mariapps.com>
2025-04-24 23:18:57 +0530

    Update chatbot_web.py

--- chatbot_web.py
+++ chatbot_web.py
@@ -24,4 +24,30 @@ if user_input:
     elif lower_input == "project owner":
         st.write("Project owners differ for CMA and BSM. Please specify which one you're asking about (CMA/BSM).")
     elif lower_input == "cma project owner":
-        st.write("The project owner for CMA ETL
+        st.write("The project owner for CMA ETL projects is Mariapps.")
+    elif lower_input == "bsm project owner":
+        st.write("The project owner for BSM ETL projects is Mariapps.")
+    elif lower_input == "etl tool used":
+        st.write("We primarily use Azure Data Factory (ADF), Azure managed instance, and Azure storage in our ETL processes.")
+    elif lower_input == "frequency of pipeline run":
+        st.write("Most of our pipelines are scheduled to run daily, with critical ones running every 4 hours.")
+    elif lower_input == "how to raise an issue":
+        st.write("You can raise issues by dropping an email to etl-support@mariapps.com or using our Jira service desk.")
+    elif lower_input == "what is cma":
+        st.write("CMA refers to our internal ETL project focused on consolidating data for reference.")
+    elif lower_input == "what is bsm":
+        st.write("BSM refers to our internal ETL project focusing on data integration and reporting.")
+    elif "cma" in lower_input:
+        st.write("What do you want to know in CMA?")
+        if st.button("Subscription"):
+            st.info("https://mariapps.sharepoint.com/:i:/s/bsm-bi/ES15Kg-qVkBKlazafQlA7ZEBgl7azTKMMTht3zny8BhM7A?e=p82fmY")
+        if st.button("Pipeline Details"):
+            st.info("CMA Link: https://mariapps.sharepoint.com/...")
+    elif "bsm" in lower_input:
+        st.write("What do you want to know in BSM?")
+        if st.button("Subscription"):
+            st.info("https://mariapps.sharepoint.com/:i:/s/bsm-bi/ER-qv25h-vhIvxw4_jVnqcoBYunBMV_Ntdhe_VvajA9DpA?e=w8eggm")
+        if st.button("Pipeline Details"):
+            st.info("BSM Link: https://mariapps.sharepoint.com/...")
+    else:
+        st.warning("Sorry I didn't get that. Try 'cma', 'bsm', or 'exit'. I'm created specifically for providing MA ETL details.")
