16ebb9321d6aee55e2602447baedacbf8a533b0e
Mariappsetl <dheeraj.balakrishnan@mariapps.com>
2025-04-24 16:24:43 +0530

    Update chatbot_web.py

--- chatbot_web.py
+++ chatbot_web.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Minimal Chatbot", layout="centered")
 
-st.image("https://www.google.co.in/imgres?q=sankar%20mariapps&imgurl=https%3A%2F%2Fbl-i.thgim.com%2Fpublic%2Fnews%2Feksad3%2Farticle54387085.ece%2Falternates%2FLANDSCAPE_1200%2Fsankar-raghavanjpg&imgrefurl=https%3A%2F%2Fwww.thehindubusinessline.com%2Fnews%2Fmariapps-marine-solutions-singapore-opens-office-in-kochi%2Farticle33578244.ece&docid=bFkfsLBXFUvX9M&tbnid=QQT05Iku_D7B8M&vet=12ahUKEwi1pLWrv_CMAxWscmwGHeAhKScQM3oECGQQAA..i&w=1200&h=675&hcb=2&itg=1&ved=2ahUKEwi1pLWrv_CMAxWscmwGHeAhKScQM3oECGQQAA", width=80)
+st.image("https://www.google.co.in/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Frobot%2F&psig=AOvVaw0zLusNqi6xkrtbtl1sFGL1&ust=1745578451115000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOjevc7A8IwDFQAAAAAdAAAAABAE", width=80)
 st.markdown("### Minimal Chatbot")
 
 user_input = st.text_input("Ask me about CMA or BSM (type 'exit' to quit):")
